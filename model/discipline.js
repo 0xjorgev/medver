3ce6345517a582ab3a32824a7c5690c5e4b19461
Jorge Mendoza <jorgevmendoza@gmail.com>
2016-02-17 10:52:23 -0430

    Catching Uppercase and lowercase changes in git

---
+++ model/discipline.js
@@ -0,0 +1,10 @@
+var bookshelf = require("../config/bookshelf");
+var subdiscipline = require('./subdiscipline');
+var discipline = bookshelf.Model.extend({
+	//Model / Table  Name
+  	tableName: 'Discipline',
+    Subdiscipline: function() {
+       return this.hasMany(Subdiscipline, 'disciplineId');
+    }
+});
+// module.export = discipline;

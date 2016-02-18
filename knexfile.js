5339387029843d53d0330f3aa63b8c94c5dbb927
Jorge Mendoza <jorgevmendoza@gmail.com>
2016-02-18 17:24:35 -0430

    Local development changes

--- knexfile.js
+++ knexfile.js
@@ -3,19 +3,22 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-        host     : process.env.DB_HOST,
-        user     : process.env.DB_USER,
-        password : process.env.DB_PASSWORD,
-        database : process.env.DB_NAME
-    }
+        host     : '127.0.0.1',
+        user     : 'postgres',
+        password : 'postgres',
+        database : 'somosport_core'
+    },
+    migrations: { tableName: 'schema_info'},
+    pool: { min:1, max:5},
+    seeds: { directory: 'seeds'}
   },
    production: {
      client: 'pg',
-     connection: process.env.DATABASE_URL
-   },
-   migrations: {
-    tableName: 'schema_info'
-  }
+     connection: process.env.DATABASE_URL,
+     migrations: { tableName: 'schema_info'},
+     pool: { min:1, max:5},
+     seeds: { directory: 'seeds'}
+   }
 };
 
 /*

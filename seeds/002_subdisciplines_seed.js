69beb18d304fa460d38325a833c3d8e5ab45b05a
Jorge Mendoza <jorgevmendoza@gmail.com>
2016-02-18 16:33:28 -0430

    fixed migration

--- seeds/002_subdisciplines_seed.js
+++ seeds/002_subdisciplines_seed.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
     knex('subdisciplines').del(),
 
     // Inserts seed entries
-    knex('subdisciplines').insert({name: 'Five-a-side football', description:" is a variation of association football in which each team fields five players (four outfield players and a goalkeeper). Other differences from football include a smaller pitch, smaller goals, and a reduced game duration. Matches are played indoors, or outdoors on AstroTurf or artificial grass pitches that may be enclosed within a barrier or cage to prevent the ball from leaving the playing area and keep the game flowing.", discipline_id:1}),
+    knex('subdisciplines').insert({name: 'Five-a-side football', description:"Is a variation of association football in which each team fields five players (four outfield players and a goalkeeper). Other differences from football include a smaller pitch, smaller goals, and ...", discipline_id:1}),
     knex('subdisciplines').insert({name: 'Baseball', description:"Association football, more commonly known as football or soccer,[3] is a sport played between two teams of eleven players with a spherical ball.", discipline_id:1}),
     knex('subdisciplines').insert({name: 'Soccer', description:"Association football, more commonly known as football or soccer,[3] is a sport played between two teams of eleven players with a spherical ball.", discipline_id:1}),
     knex('subdisciplines').insert({name: 'Beach Soccer', description:"Beach soccer, also known as beach football or beasal, is a variant of association football played on a beach or some form of sand.", discipline_id:1}),

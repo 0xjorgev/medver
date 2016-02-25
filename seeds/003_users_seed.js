c7734fad3a54a3efe0f2ccc1b184ceafed4efb7f
Jorge Mendoza <jorgevmendoza@gmail.com>
2016-02-25 17:40:37 -0430

    Added new field to disciple and subdiscipline and added new data in the seeds

--- seeds/003_users_seed.js
+++ seeds/003_users_seed.js
@@ -4,7 +4,8 @@ exports.seed = function(knex, Promise) {
     knex('users').del(),
 
     // Inserts seed entries
-    knex('users').insert({username:'jorge', password:'123456', email:'jorgevmendoza@gmail.com'}),
-    knex('users').insert({username:'demo', password:'123456', email:'demo@somosport.com'})
+    knex('users').insert({username:'jorge', password:'e10adc3949ba59abbe56e057f20f883e', email:'jorgevmendoza@gmail.com'}),
+    knex('users').insert({username:'demo', password:'e10adc3949ba59abbe56e057f20f883e', email:'demo@somosport.com'}),
+    knex('users').insert({username:'fran', password:'043219f7b72865e3e59e282c39e1a937', email:'franciscodlb@codefuel.me'})
     );
 };

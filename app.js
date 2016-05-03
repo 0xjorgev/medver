fca57c5ee574fa980b9505e8710e36eeaf294efc
Ramsés López <ramses.lopezs@gmail.com>
2016-05-03 14:32:20 -0400

    Se agrega content-type entre los headers permitidos

--- app.js
+++ app.js
@@ -50,10 +50,9 @@
   var allowCrossDomain = function(req, res, next) {
   	res.header('Access-Control-Allow-Origin', '*');
     res.header('origins','*');
-    // res.header('origins','https://herokuapp.com:* https://somosportpocdev.herokuapp.com:* https://somosport-competition-dev.herokuapp.com:*');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Content-Type', 'application/json');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
   	next();
   };
 

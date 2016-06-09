7020118b7588c892046b21147cc8dd9ff859352f
Jorge Mendoza <jorgevmendoza@gmail.com>
2016-06-09 17:07:50 -0400

    Fixed 🐞🐛 on seeds

---
+++ migrations/20160609222257_standing_table.js
@@ -0,0 +1,24 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+		knex.schema.createTable('standing_tables', function(table){
+			table.increments('id').primary();
+			table.boolean('active').notNullable().defaultTo(true);
+			table.timestamp('created_at').defaultTo(knex.fn.now());
+			table.timestamp('updated_at').defaultTo(knex.fn.now());
+			//Relationships
+			// table.integer('team_id').references('teams.id').index();
+			// table.integer('category_id').references('categories.id').index();
+			// table.integer('group_id').references('groups.id').index();
+			// table.integer('phase_id').references('phases.id').index();
+		})
+		.then(function(){
+			//Add query
+            return knex.raw('');
+        })
+	]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([ knex.schema.dropTableIfExists('standing_tables') ]);
+};

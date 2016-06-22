c3803f8f293e37822d6e1dc274735e8ab2dac13b
Ramsés López <ramses.lopezs@gmail.com>
2016-06-22 17:21:02 -0400

    portait_url a la migracion de player y number a la migracion de matches

--- migrations/20160420214740_matches.js
+++ migrations/20160420214740_matches.js
@@ -4,6 +4,7 @@ exports.up = function(knex, Promise) {
 		knex.schema.createTable('matches', function(table){
 			table.increments('id').primary();
 			table.text('location');
+			table.integer('number');
 			table.integer('home_team_id').references('teams.id').index();
       		table.integer('home_team_score');
 			table.integer('visitor_team_id').references('teams.id').index();

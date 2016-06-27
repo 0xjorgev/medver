964f1270b3dd3667592df949d23896a65b1cdcdd
Ramsés López <ramses.lopezs@gmail.com>
2016-06-27 14:52:15 -0400

    Script para taggear seed files

--- tag_seed_files.rb
+++ tag_seed_files.rb
@@ -1,5 +1,12 @@
-Dir.foreach('./seeds/') do |item|
+
+dirname = './seeds/'
+
+Dir.foreach(dirname) do |item|
   next if item == '.' or item == '..'
-  puts item
+
+  File.open(dirname + item, 'a') do |f|
+  	f << 'console.log(__filename.slice(__dirname.length + 1) + \'OK\')'
+  end
+
 end
 

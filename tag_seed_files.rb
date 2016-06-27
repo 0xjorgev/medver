07330cbce1eb5ae47d615167a3fedb36b8ccb915
Ramsés López <ramses.lopezs@gmail.com>
2016-06-27 15:14:41 -0400

    fix en tagging script

--- tag_seed_files.rb
+++ tag_seed_files.rb
@@ -1,12 +1,15 @@
 
 dirname = './seeds/'
+footer = "\nconsole.log(__filename.slice(__dirname.length + 1) + 'OK')\n"
 
 Dir.foreach(dirname) do |item|
-  next if item == '.' or item == '..'
+	next if item == '.' or item == '..'
 
-  File.open(dirname + item, 'a') do |f|
-  	f << 'console.log(__filename.slice(__dirname.length + 1) + \'OK\')'
-  end
+	text = File.read(dirname + item)
+
+	unless text.include?(footer)
+		File.open(dirname + item, 'a') {|f| f << footer}
+	end
 
 end
 

18e11e4660a275fba0714b7c7abbded637bfa072
Jorge Mendoza <jorgevmendoza@gmail.com>
2016-07-06 11:38:48 -0400

    nuke db modification for knex on both global and local

--- nuke_db.sh
+++ nuke_db.sh
@@ -1,1 +1,1 @@
-PGUSER=postgres dropdb somosport_core && PGUSER=postgres createdb somosport_core && knex migrate:latest && knex seed:run
+PGUSER=postgres dropdb somosport_core && PGUSER=postgres createdb somosport_core && $(npm bin)/knex migrate:latest && $(npm bin)/knex seed:run

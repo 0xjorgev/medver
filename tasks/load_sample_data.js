f41ec031f9825c6e60aecaee1a8a82396f168505
Ramsés López <ramses.lopezs@gmail.com>
2016-07-22 15:33:41 -0400

    prueba de config de DB

--- tasks/load_sample_data.js
+++ tasks/load_sample_data.js
@@ -6,7 +6,11 @@ var Config = require('../knexfile.js')
 var Models = require('../model/index.js')
 var knex = new Knex(process.env.NODE_ENV || Config.development);
 
+
 var _log = (obj) => console.log(inspect(obj, { colors: true, depth: Infinity }))
+
+
+_log(Config)
 
 //read file
 const data = xlsx.parse(`${__dirname}/SabanaCompTestV1.xlsx`)

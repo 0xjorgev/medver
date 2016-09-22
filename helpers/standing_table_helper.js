85b5037d4537ebb73eac112f59491a39f43792a0
Ramsés López <ramses.lopezs@gmail.com>
2016-09-22 14:32:47 -0400

    Standing table helper promisified

--- helpers/standing_table_helper.js
+++ helpers/standing_table_helper.js
@@ -2,18 +2,16 @@ if (typeof define !== 'function') {
 	var define = require('amdefine')(module);
 }
 
-// var _ = require('lodash')
-
 define(['../util/knex_util',
 	'../node_modules/lodash/lodash.min',
 	'../model/index',
 	'../util/request_message_util',
 	'../util/response_message_util'
 	],
-	function(Knex, _,
-		Models,
-		Message,
-		Response){
+	(Knex, _,
+	Models,
+	Message,
+	Response) => {
 
 	var StandingTable = {}
 
@@ -22,9 +20,9 @@ define(['../util/knex_util',
 	//==========================================================================
 
 	//dados los eventos de un match, se asignan los goles del evento al match por cada equipo, home o visitor
-	var assignGoalsToTeams = function(events){
-		return function(match){
-			events.forEach(function(event){
+	var assignGoalsToTeams = (events) => {
+		return (match) => {
+			events.forEach((event) => {
 				if(event.match_id == match.match_id){
 					match.home_team_goals = (event.team_id == match.home_team_id) ? event.goals : match.home_team_goals
 					match.visitor_team_goals = (event.team_id == match.visitor_team_id) ? event.goals : match.visitor_team_goals
@@ -37,7 +35,7 @@ define(['../util/knex_util',
 	}
 
 	//dado un match, se le coloca dos propiedades para los scores de cada equipo
-	var prepMatch = function(match){
+	var prepMatch = (match) => {
 		match.home_team_goals = 0
 		match.visitor_team_goals = 0
 		match.total_goals = 0
@@ -45,14 +43,14 @@ define(['../util/knex_util',
 	}
 
 	//filtro que obtiene los eventos dado un match
-	var filterByMatch = function(match){
-		return function(event){
+	var filterByMatch = (match) => {
+		return (event) => {
 			return event.match_id == match.id
 		}
 	}
 
 	//TODO: point value should be taken from db
-	var assignPointsByMatch = function(m){
+	var assignPointsByMatch = (m) => {
 		if(m.home_team_goals == m.visitor_team_goals){
 			m.home_team_points = 1
 			m.visitor_team_points = 1
@@ -65,14 +63,11 @@ define(['../util/knex_util',
 	}
 
 	//sacar todos los ids de equipo, en home y visitor, then uniq
-	var buildStandingTable = function(table,match){
-		table.find(function(team){
-			return team.team_id == match.home_team_id
-		})
+	var buildStandingTable = (table, match) => {
+		table.find((team) => team.team_id == match.home_team_id )
 	}
 
-	var setTeamResults = function(team, match){
-
+	var setTeamResults = (team, match) => {
 		var result = {team_id: team.id, data: team, points: 0,
 		goals_in_favor: 0, goals_against: 0,
 		matches_won: 0, matches_lost: 0,
@@ -80,15 +75,14 @@ define(['../util/knex_util',
 
 		//si el equipo participó en el match
 		if(match.home_team_id == team.id || match.visitor_team_id == team.id){
-
-			//el team es home
 			if(match.home_team_id == team.id){
+				//el team es home
 				result.points = match.home_team_points
 				result.goals_in_favor = match.home_team_goals
 				result.goals_against = match.visitor_team_goals
 			}
-			//team es visitor
 			else{
+				//team es visitor
 				result.points = match.visitor_team_points
 				result.goals_in_favor = match.visitor_team_goals
 				result.goals_against = match.home_team_goals
@@ -109,16 +103,16 @@ define(['../util/knex_util',
 	}
 
 	//se extraen los matches donde ha participado el equipo y se normaliza en una estructura estandar
-	var normalizeTeamResults = function(matchesWithResults){
-		return function(team){
-			return matchesWithResults.map(function(match){
+	var normalizeTeamResults = (matchesWithResults) => {
+		return (team) => {
+			return matchesWithResults.map((match) => {
 				return setTeamResults(team, match)
 			})
 		}
 	}
 
-	var calculateStandingTable = function(results){
-		return results.reduce(function(total, result){
+	var calculateStandingTable = (results) => {
+		return results.reduce((total, result) => {
 			total.team_id = result.team_id
 			total.points += result.points
 			total.goals_in_favor += result.goals_in_favor
@@ -127,7 +121,6 @@ define(['../util/knex_util',
 			total.matches_lost += result.matches_lost
 			total.matches += result.matches
 			total.matches_draw += result.matches_draw
-
 			total.data = result.data
 			return total
 		},{team_id: null, points: 0, goals_in_favor: 0, goals_against: 0, matches: 0, matches_won: 0, matches_lost: 0, matches_draw: 0})
@@ -137,18 +130,16 @@ define(['../util/knex_util',
 	// 1 - ganó el partido
 	// -1 ganó el contrario
 	// 0 empate
-	var getTeamResult = function(team, matchResult){
+	var getTeamResult = (team, matchResult) => {
 		var result = null
 		if(matchResult.home_team_goals == matchResult.visitor_team_goals){
 			result = 0
 		}
 		else{
-			if(team.id == matchResult.home_team_id){
+			if(team.id == matchResult.home_team_id)
 				result = matchResult.home_team_goals > matchResult.visitor_team_goals ? 1 : -1
-			}
-			else{
+			else
 				result = matchResult.visitor_team_goals > matchResult.home_team_goals ? 1 : -1
-			}
 		}
 
 		return result
@@ -160,47 +151,60 @@ define(['../util/knex_util',
 
 	//given a set of matches (could be a entire category, or a group), returns the current standing table
 
-	StandingTable.getStandingTableByMatches = function(matchSql, res){
+	StandingTable.getStandingTableByMatches = (matchSql, res) => {
+		var matchesWithResults = undefined
+		var matches = undefined
 
-		var matchesByCategory = Knex.raw(matchSql)
+		// se obtienen los matches que pertenecen a la categoria o grupo
+		// la diferencia viene del query 'matchSql'; es ahi donde se diferencia si buscamos los matches de una cat o group
+		Knex.raw(matchSql)
 		.then((result) => {
-			var matches = result.rows
+			//Se extraen los matches de la estructura y se almacenan en matches para uso posterior
+			matches = result.rows
+
+			//si la estructura no tiene matches, se retorna 404
 			if(!matches || matches.length == 0){
-				Message(res, 'No matches found', 404, '')
+				Response(res, matches) // matches está vacio y causa un 404
 				return
 			}
 
+			//separo los IDs de los matches
 			var matchIds = matches.map((e) => e.match_id).join(',')
+
+			//query para obtener el # de goles de los matches
 			var goalsByMatchSQL = `select match_id, event_id, team_id, count(*) as goals from events_matches_players where active = true and event_id = 1 and match_id in (${matchIds}) group by 1,2,3 order by 1,2,3`
-			var standing_table = Knex.raw(goalsByMatchSQL).then((result) => {
-				goals = result.rows
-				goals.map((g) => g.goals = parseInt(g.goals))
 
-				var matchesWithResults = matches
-					.map(prepMatch)
-					.map(assignGoalsToTeams(goals))
-					.map(assignPointsByMatch)
+			return Knex.raw(goalsByMatchSQL)
+		})
+		.then((result) => {
+			var goals = result.rows
+			goals.map((g) => g.goals = parseInt(g.goals))
 
-				//extraigo los ID de teams
-				var teams = matchesWithResults.map((m) => [m.home_team_id, m.visitor_team_id])
-				teams = _(teams).flatten().uniq().value()
+			//estos son los matches con los puntos ya determinados
+			matchesWithResults = matches
+				.map(prepMatch)
+				.map(assignGoalsToTeams(goals))
+				.map(assignPointsByMatch)
 
-				return Models.team
-					.where({active: true})
-					.where('id', 'in', teams)
-					.fetchAll({withRelated: ['category_type', 'organization', 'player_team.player'], debug: false})
-			})
-			.then((result) => {
-				teams = result.models.map((m) => m.attributes)
-				//se sumarizan los resultados normalizados de los partidos
-				var standingTable = teams
-					.map(normalizeTeamResults(matchesWithResults))
-					.map(calculateStandingTable)
-				Response(res, standingTable)
-			})
-			.catch((error) => {
-				Response(res, null, error)
-			})
+			//extraigo los ID de teams
+			var teams = matchesWithResults.map((m) => [m.home_team_id, m.visitor_team_id])
+			teams = _(teams).flatten().uniq().value()
+
+			return Models.team
+				.where({active: true})
+				.where('id', 'in', teams)
+				.fetchAll({withRelated: ['category_type', 'organization', 'player_team.player'], debug: false})
+		})
+		.then((result) => {
+			teams = result.models.map((m) => m.attributes)
+			//se sumarizan los resultados normalizados de los partidos
+			var standingTable = teams
+				.map(normalizeTeamResults(matchesWithResults))
+				.map(calculateStandingTable)
+			Response(res, standingTable)
+		})
+		.catch((error) => {
+			Response(res, null, error)
 		})
 	}
 

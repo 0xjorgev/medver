edefec913c0f13bb7173306f7aaf277f93f040bc
chesco <franciscoj.delablancam@gmail.com>
2016-10-25 17:15:05 -0400

    Se cambiaron los roles a los indicados en el documento funcional

--- seeds/0012_roles.js
+++ seeds/0012_roles.js
@@ -6,11 +6,12 @@ exports.seed = function(knex, Promise) {
     // Deletes ALL existing entries
     knex('roles').del(),
     knex('roles').insert({description: 'owner'}),
-    knex('roles').insert({description: 'belongsTo'}),
-    knex('roles').insert({description: 'administrator'}),
+    knex('roles').insert({description: 'admin'}),
+    knex('roles').insert({description: 'member'}),
+    knex('roles').insert({description: 'referee'}),
+    knex('roles').insert({description: 'couch'}),
     knex('roles').insert({description: 'follower'}),
-    knex('roles').insert({description: 'fan'}),
-    knex('roles').insert({description: 'afilliate'})
+    knex('roles').insert({description: 'ss'})
     ).then(function(){
 		//Add query
         // return knex.raw('update roles_sequence_id ');

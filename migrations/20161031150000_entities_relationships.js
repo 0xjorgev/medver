6f3aeb5376ce08cf5cfab0dda9eacffa42414cd1
Jorge Mendoza <jorgevmendoza@gmail.com>
2016-10-31 15:00:46 -0400

    Fixes on entitties_relationships

--- migrations/20161031150000_entities_relationships.js
+++ migrations/20161031150000_entities_relationships.js
@@ -3,9 +3,9 @@ exports.up = function(knex, Promise) {
 	return Promise.all([
 		knex.schema.createTable('entities_relationships', function(table){
 			table.increments('id').primary();
-			table.integer('ent_ref_from_id');
+			table.integer('ent_ref_from_id').references('entities.id').index();
+      table.integer('ent_ref_to_id').references('entities.id').index();
 			table.integer('relationship_type').references('relationships_types.id').index();
-			table.integer('ent_ref_to_id');
 			table.text('comment');
 			table.boolean('active').notNullable().defaultTo(true);
 			table.integer('created_by').references('users.id').index();

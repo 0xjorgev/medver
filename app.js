249fd5555427a8603b156dc8b5d02f60c6875ae3
Ramsés López <ramses.lopezs@gmail.com>
2017-01-26 14:47:12 -0400

    Implementacion de redis para estaditicas; cambios para adaptar app.js a ES6; logger_utils ahora utiliza lme para estilar logs

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 //skips newrelic if not in heroku
 if(process.env.NODE_ENV == 'production') require('newrelic')
 
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var morgan = require('morgan'); //nodejs logger
-var nJwt = require('njwt') //jwt token generator
-var Message = require('./util/request_message_util') //in-house message handler
-var Response = require('./util/response_message_util') //in-house message handler
-var compression = require('compression');
-var argv = require('minimist')(process.argv.slice(2));
-var subpath = express();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan'); //nodejs logger
+const nJwt = require('njwt') //jwt token generator
+const Message = require('./util/request_message_util') //in-house message handler
+const Response = require('./util/response_message_util') //in-house message handler
+const compression = require('compression');
+const argv = require('minimist')(process.argv.slice(2));
+const subpath = express();
 const logger = require('./util/logger_util')
 
 //node port
-var port = process.env.PORT;
-
+const port = process.env.PORT;
 app.use(compression());
-
 app.use("/v1", subpath);
 
 //==========================================================================
@@ -26,8 +24,8 @@ app.use("/v1", subpath);
 //==========================================================================
 
 // swagger.setAppHandler(subpath);
-var swagger_node_express = require("swagger-node-express");
-var swagger = swagger_node_express.createNew(app);
+const swagger_node_express = require("swagger-node-express");
+const swagger = swagger_node_express.createNew(app);
 
 app.use(express.static('docs'));
 
@@ -46,7 +44,7 @@ app.get('/', (req, res) => res.sendFile(__dirname + '/docs/index.html') );
 swagger.configureSwaggerPaths('', 'api-docs', 'docs');
 
 // // Configure the API domain
-var domain = 'localhost';
+let domain = 'localhost';
 if(argv.domain !== undefined)
     domain = argv.domain;
 else{
@@ -66,76 +64,73 @@ var applicationUrl = 'http://' + domain + ':' + port;
 
 swagger.configure(applicationUrl, '1.0.0');
 
-
 //==========================================================================
 // end swagger stuff
 //==========================================================================
 
-var discipline_ws = require('./route/disciplines_route');
-var user_ws = require('./route/users_route');
-var competition_ws = require('./route/competitions_route');
-var season_ws = require('./route/seasons_route');
-var category_ws = require('./route/categories_route');
-var gender_ws = require('./route/genders_route');
-var phase_ws = require('./route/phases_route');
-var group_ws = require('./route/groups_route');
-var round_ws = require('./route/rounds_route');
-var match_ws = require('./route/matches_route');
-var team_ws = require('./route/teams_route');
-var event_ws = require('./route/results_route');
-var referee_ws = require('./route/referees_route');
-var category_type_ws = require('./route/categories_types_route');
-var player_ws = require('./route/players_route');
-var rule_ws = require('./route/rules_route');
-var match_event_ws = require('./route/events_route');
-var subdiscipline_ws = require('./route/subdisciplines_route');
-var test_ws =  require('./route/tests_route');
-var position_ws =  require('./route/positions_route');
-var status_type_ws =  require('./route/status_types_route');
-var request_ws =  require('./route/requests_route');
+const discipline_ws = require('./route/disciplines_route');
+const user_ws = require('./route/users_route');
+const competition_ws = require('./route/competitions_route');
+const season_ws = require('./route/seasons_route');
+const category_ws = require('./route/categories_route');
+const gender_ws = require('./route/genders_route');
+const phase_ws = require('./route/phases_route');
+const group_ws = require('./route/groups_route');
+const round_ws = require('./route/rounds_route');
+const match_ws = require('./route/matches_route');
+const team_ws = require('./route/teams_route');
+const event_ws = require('./route/results_route');
+const referee_ws = require('./route/referees_route');
+const category_type_ws = require('./route/categories_types_route');
+const player_ws = require('./route/players_route');
+const rule_ws = require('./route/rules_route');
+const match_event_ws = require('./route/events_route');
+const subdiscipline_ws = require('./route/subdisciplines_route');
+const test_ws =  require('./route/tests_route');
+const position_ws =  require('./route/positions_route');
+const status_type_ws =  require('./route/status_types_route');
+const request_ws =  require('./route/requests_route');
 const feed_item_ws = require('./route/feed_items_route')
 const club_ws = require('./route/clubs_route')
-
 //put here all non business-related services
-var core_ws =  require('./route/core_route');
-
-var apiVersion = 'v1.0';
-var prefix = 'api';
-var api_prefix = `/${prefix}/${apiVersion}/`; //'template literal syntax' is only available in ES6 (use 'esversion: 6').
+const core_ws =  require('./route/core_route');
+const apiVersion = 'v1.0';
+const prefix = 'api';
+const api_prefix = `/${prefix}/${apiVersion}/`; //'template literal syntax' is only available in ES6 (use 'esversion: 6').
 
 //routes names
-var routes = {
-	discipline      : 'discipline'
-	,competition    : 'competition'
-	,season         : 'season'
-	,category       : 'category'
-	,gender         : 'gender'
-	,contact        : 'contact'
-	,organization   : 'organization'
-	,phase          : 'phase'
-	,group          : 'group'
-	,round          : 'round'
-	,team           : 'team'
-	,match          : 'match'
-	,rule           : 'rule'
-	,ruleset        : 'ruleset'
-	,event          : 'event'
-	,referee        : 'referee'
-	,category_type  : 'category_type'
-	,feed_item  	: 'feed_item'
-	,player         : 'player'
-	,rule           : 'rule'
-	,match_event    : 'matchevent'
-	,subdiscipline  : 'subdiscipline'
-	,test           : 'test'
-	,position       : 'position'
-	,status_type    : 'status_type'
-	,request        : 'request'
-	,club        	: 'club'
+const routes = {
+	discipline		: 'discipline'
+	,competition	: 'competition'
+	,season			: 'season'
+	,category		: 'category'
+	,gender			: 'gender'
+	,contact		: 'contact'
+	,organization	: 'organization'
+	,phase			: 'phase'
+	,group			: 'group'
+	,round			: 'round'
+	,team			: 'team'
+	,match			: 'match'
+	,rule			: 'rule'
+	,ruleset		: 'ruleset'
+	,event			: 'event'
+	,referee		: 'referee'
+	,category_type	: 'category_type'
+	,feed_item		: 'feed_item'
+	,player			: 'player'
+	,rule			: 'rule'
+	,match_event	: 'matchevent'
+	,subdiscipline	: 'subdiscipline'
+	,test			: 'test'
+	,position		: 'position'
+	,status_type	: 'status_type'
+	,request		: 'request'
+	,club			: 'club'
 }
 
 //Middleware
-var allowCrossDomain = function(req, res, next) {
+const allowCrossDomain = function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('origins','*');
 	res.header('Access-Control-Allow-Credentials', 'true');
@@ -188,23 +183,23 @@ const validateToken = function (req, res, next) {
 
 app.use(validateToken)
 
-  /******
-  * para procesamiento de query strings, como ordenamiento
-  * se espera recibir un parametro con la forma:
-  * <service_url>/<resource>?sort=-priority,created_at,+updated_at
-  * signo + asc, - desc
-  * si se envia el campo sin un signo + o - se asume +
-  * esta funcion coloca en el request un array con los campos del sort;
-  * sin embargo, es responsabilidad de la implementacion del servicio realizar
-  * los ajustes necesarios a la consulta
-  *******/
+/******
+* para procesamiento de query strings, como ordenamiento
+* se espera recibir un parametro con la forma:
+* <service_url>/<resource>?sort=-priority,created_at,+updated_at
+* signo + asc, - desc
+* si se envia el campo sin un signo + o - se asume +
+* esta funcion coloca en el request un array con los campos del sort;
+* sin embargo, es responsabilidad de la implementacion del servicio realizar
+* los ajustes necesarios a la consulta
+*******/
 const processSortBy = (req, res, next) => {
 	if(!req.query['sort']){
 		next()
 		return
 	}
 
-	var sort = req.query['sort']
+	const sort = req.query['sort']
 	req._sortBy = sort.split(',').map((s) => {
 		return (s.indexOf('-') >= 0)
 			? [s.replace('-',''),'desc']
@@ -255,13 +250,11 @@ app.use(`${api_prefix}${routes.club}`, club_ws);
 
 app.get(api_prefix, function(request, response){
 	// app._router.stack.forEach(function(r){
-	//   if (r.route && r.route.path){
-	//     console.log(r.route.path)
-	//   }
+	// 	if (r.route && r.route.path){
+	// 		console.log(r.route.path)
+	// 	}
 	// });
-	response.send({api:'api'});
+	response.send({api:'api'})
 });
 
-app.listen(port, function(){
-	// console.log('Running ' + process.env.NODE_ENV +' on port ' + port);
-});
+app.listen(port, function(){})

f512babc87320204313f8ba6a40f1d002f191708
Jorge Mendoza <jorgevmendoza@gmail.com>
2017-02-06 17:24:11 -0400

    Tesging JSON parser

--- route/paypal_route.js
+++ route/paypal_route.js
@@ -27,12 +27,13 @@ define(['express'
     //=========================================================================
     router.post('/', (req, res) => {
       var body = req.body;
-      var cat_id = body.custom.category_id;
-      var team_id = body.custom.team_id;
+      var json = JSON.parse(body.custom);
+      var cat_id = json.category_id;
+      var team_id = json.team_id;
       //Add Search for Competition
       //Update the team - Competition payment status
       console.log("*****************************");
-      console.log("payPal Response:", body.custom, "cat:", cat_id, "team:", team_id);
+      console.log("payPal Response:", , "cat:", cat_id, "team:", team_id);
       console.log("*****************************");
       logger.debug(Object.keys(req));
       console.log("*****************************");

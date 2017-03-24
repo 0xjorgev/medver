a5e1fb2e0085cb8d88af0ee3aa284e26d77b6950
Ramsés López <ramses.lopezs@gmail.com>
2017-03-24 12:19:57 -0400

    Correccion en migacion

--- migrations/20170323154201_add_document_img_url_to_player.js
+++ migrations/20170323154201_add_document_img_url_to_player.js
@@ -1,12 +1,15 @@
-
 exports.up = function(knex, Promise) {
-	knex.schema.alterTable('players', (table) => {
-		table.string('document_img_url')
-	})
-};
+	return Promise.all([
+		knex.schema.alterTable('players', (table) => {
+			table.string('document_img_url')
+		})
+	])
+}
 
 exports.down = function(knex, Promise) {
-	knex.schema.alterTable('players', (table) => {
-		table.dropColumn('document_img_url')
-	})
-};
+	return Promise.all([
+		knex.schema.alterTable('players', (table) => {
+			// table.dropColumn('document_img_url')
+		})
+	])
+}

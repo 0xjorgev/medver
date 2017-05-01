5bd0bcc2a8a4a685061c5d5a2794003ba5150f84
Ramsés López <ramses.lopezs@gmail.com>
2017-05-01 18:12:54 -0400

    test sendgrid 5

--- util/email_sender_util.js
+++ util/email_sender_util.js
@@ -15,6 +15,32 @@ define(['sendgrid', './logger_util'], function (Email, logger) {
 				if (err) { return console.error('Email Error:', err); }
 				console.log(`Email sent:`, json);
 			});
+
+			/*
+			//sendgrid 5.0.0 syntax
+			var helper = require('sendgrid').mail;
+			var fromEmail = new helper.Email('test@example.com');
+			var toEmail = new helper.Email('test@example.com');
+			var subject = 'Hello World from the SendGrid Node.js Library!';
+			var content = new helper.Content('text/plain', 'Hello, Email!');
+			var mail = new helper.Mail(fromEmail, subject, toEmail, content);
+
+			var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+			var request = sg.emptyRequest({
+				method: 'POST',
+				path: '/v3/mail/send',
+				body: mail.toJSON()
+			});
+
+			sg.API(request, function (error, response) {
+				if (error) {
+					console.log('Error response received');
+				}
+				console.log(response.statusCode);
+				console.log(response.body);
+				console.log(response.headers);
+			})
+			*/
 		}
 	}
 

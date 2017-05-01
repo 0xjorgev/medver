e594a2f37d29e032e6560fbf263a1408fab05a71
Ramsés López <ramses.lopezs@gmail.com>
2017-05-01 18:47:47 -0400

    Validacion para cuando string_templeate es undefined

--- util/object_map_util.js
+++ util/object_map_util.js
@@ -1,31 +1,35 @@
 if (typeof define !== 'function')
-    var define = require('amdefine')(module)
+	var define = require('amdefine')(module)
 
 define([], () => {
-  //===============================================================
-  // Replaces values of the object's key in the string_template
-  //===============================================================
+	//===============================================================
+	// Replaces values of the object's key in the string_template
+	//===============================================================
 
-  String.prototype.replaceAll = function(search, replacement) {
-    var target = this;
-    return target.split(search).join(replacement);
-  };
+	String.prototype.replaceAll = function(search, replacement) {
+		var target = this;
+		return target.split(search).join(replacement);
+	};
 
-  var keyValueReplace = (obj, string_template) => {
-    if (obj != undefined && obj != null) {
+	var keyValueReplace = (obj, string_template) => {
+		if (obj != undefined && obj != null) {
 
-        const object_map = (_obj) => {
-          return (key) => {
-              string_template =  string_template.replaceAll(key, _obj[key])
-          }
-        }
-        Object.keys(obj).map( object_map(obj) )
-        return string_template
-    } else {
-      return string_template
-    }
-  }
+			const object_map = (_obj) => {
+				return (key) => {
+					if(string_template != null && string_template != undefined){
+						string_template =  string_template.replaceAll(key, _obj[key])
+					}
+					else {
+						console.error('string_template is null or undefined', string_template)
+					}
+				}
+			}
+			Object.keys(obj).map( object_map(obj) )
+			return string_template
+		} else {
+			return string_template
+		}
+	}
 
-  return keyValueReplace
-
+	return keyValueReplace
 })
